f613ed33894c25f79268e91fa265cc197bb61cfe
Kulpot <kulpotwahu@gmail.com>
2023-09-14 20:31:54 +0800

    Add project files.

---
+++ Entity Framework SQL Query Generated/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+
+//ref link:https://www.youtube.com/watch?v=ZCBIoI5qz4M&list=PLRwVmtr-pp06bXl6mbwDfK1eW9sAIvWUZ&index=2
+//
+
+class Video
+{
+    public int ID { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+}
+
+class MeContext : DbContext
+{
+    public MeContext() : base(@"Data Source=.;Initial Catalog=KulpotDB;Integrated Security=True")
+    {
+
+    }
+    public DbSet<Video> Videos { get; set; }
+}
+
+class MainClass
+{
+    static void Main()
+    {
+        var vid = new Video
+        {
+            Title = "Hello World Entity Framework",
+            Description = "Learn about the entity framework"
+        };
+        var meContext = new MeContext();
+        meContext.Videos.Add(vid);
+        meContext.SaveChanges();
+    }
+}
